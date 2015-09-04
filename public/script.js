da6d4dbcc876a58977a284eeff08e76d20195d61
Valerie Sharp <Valeriesharp93@gmail.com>
2015-09-04 14:59:57 -0700

    made the results show the #

--- public/script.js
+++ public/script.js
@@ -2,33 +2,23 @@
 But for multiples of three print “Fizz” instead of the number  
 For the multiples of five print “Buzz”. 
 For numbers which are multiples of both three and five print “FizzBuzz”.*/
+// try to make it show the value of i 
 
-// THIS ONE IS WRONG BECAUSE OF THE ORDER 
-// for (var i = 1; i <= 100; i++) {
-// 		if (i % 3 === 0) {
-// 			document.write('Fizz'+'<br>'+i);
-// 		}	
-// 		else if (i % 5 === 0) {
-// 			document.write('buzz'+'<br>'+i);
-// 		}
-// 		else if (i % 5 === 0 && i % 3 === 0) {
-// 			document.write('FizzBuzz'+'<br>'+i);
-// 		}
-// };
 
 for (var i = 1; i <= 100; i++) {
 	var numValue = i.value;
 	if (i % 3 === 0 && i % 5 === 0) {
-		document.write('FizzBuzz'+' ');
+		document.write('FizzBuzz because '+i+' '+'<br>');
 	}
 	else if (i % 3 === 0) {
-		document.write('Fizz'+' ');
+		document.write('Fizz because '+i+' '+'<br>');
 	}
 	else if (i % 5 === 0 ) {
-	document.write('Buzz'+' ');
+	document.write('Buzz because '+i+' '+'<br>');
 	}
 	else{
-		document.write(i + ' ');
+		document.write(i + ' '+'<br>');
 	}
 };
+
 
